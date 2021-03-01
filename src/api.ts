7c92bab6375bc5b3e6d5a8e0525e902aafe4590a
Alexander Kalinin <alex@kalinin.uk>
2021-03-01 20:26:14 +0000

    add flashing

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { cli } from 'cli-ux';
 
 type SonoffInfo = {
   switch: string;
@@ -8,6 +9,10 @@ type SonoffInfo = {
   ssid: string;
   otaUnlock: boolean;
 };
+
+function getBaseURL(device: Device) {
+  return `http://${device.address}:${device.service.port}/zeroconf`;
+}
 
 export function getDeviceId(device: Device): string {
   return device.fqdn.split('.')[0].replace('eWeLink_', '');
@@ -22,4 +27,29 @@ export async function getInfo(device: Device) {
     },
   );
   return JSON.parse(res.data.data) as SonoffInfo;
+}
+
+export async function unlockOTA(device: Device) {
+  cli.action.start('Unlocking OTA');
+  await axios.post(`${getBaseURL(device)}/ota_unlock`, {
+    deviceId: getDeviceId(device),
+    data: {},
+  });
+  cli.action.stop();
+}
+
+export async function otaFlash(
+  device: Device,
+  downloadUrl: string,
+  sha256sum: string,
+) {
+  cli.action.start('Sending flashing details');
+  await axios.post(`${getBaseURL(device)}/ota_flash`, {
+    deviceId: getDeviceId(device),
+    data: {
+      downloadUrl,
+      sha256sum,
+    },
+  });
+  cli.action.stop();
 }
