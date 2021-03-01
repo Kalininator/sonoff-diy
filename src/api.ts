23a324c4fe919b5abaf83d9883687963c84bb03b
Alexander Kalinin <alex@kalinin.uk>
2021-03-01 21:56:17 +0000

    Dev (#4)
    * initial setup and identify command
    
    * Update index.ts
    
    * fix checks
    
    * add publish workflow
    
    * Update publish.yml
    
    * make nicer
    
    * add list command
    
    * fix getDeviceId
    
    * add flashing
    
    * Update publish.yml
    
    * add extra log at end
    
    * add ip/host flags

--- src/api.ts
+++ src/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { cli } from 'cli-ux';
 
-type SonoffInfo = {
+export type SonoffInfo = {
   switch: string;
   startup: string;
   pulse: string;
@@ -10,42 +10,51 @@ type SonoffInfo = {
   otaUnlock: boolean;
 };
 
-function getBaseURL(device: Device) {
-  return `http://${device.address}:${device.service.port}/zeroconf`;
+export type DeviceInfo = {
+  address: string;
+  port: number;
+};
+
+function getBaseURL(device: DeviceInfo) {
+  return `http://${device.address}:${device.port}/zeroconf`;
 }
 
 export function getDeviceId(device: Device): string {
   return device.fqdn.split('.')[0].replace('eWeLink_', '');
 }
 
-export async function getInfo(device: Device) {
-  const res = await axios.post(
-    `http://${device.address}:${device.service.port}/zeroconf/info`,
-    {
-      deviceId: getDeviceId(device),
-      data: {},
-    },
-  );
+export function getDeviceInfo(device: Device): DeviceInfo {
+  return {
+    address: device.address,
+    port: device.service.port,
+  };
+}
+
+export async function getInfo(device: DeviceInfo) {
+  const res = await axios.post(`${getBaseURL(device)}/info`, {
+    deviceId: '',
+    data: {},
+  });
   return JSON.parse(res.data.data) as SonoffInfo;
 }
 
-export async function unlockOTA(device: Device) {
+export async function unlockOTA(device: DeviceInfo) {
   cli.action.start('Unlocking OTA');
   await axios.post(`${getBaseURL(device)}/ota_unlock`, {
-    deviceId: getDeviceId(device),
+    deviceId: '',
     data: {},
   });
   cli.action.stop();
 }
 
 export async function otaFlash(
-  device: Device,
+  device: DeviceInfo,
   downloadUrl: string,
   sha256sum: string,
 ) {
   cli.action.start('Sending flashing details');
   await axios.post(`${getBaseURL(device)}/ota_flash`, {
-    deviceId: getDeviceId(device),
+    deviceId: '',
     data: {
       downloadUrl,
       sha256sum,
